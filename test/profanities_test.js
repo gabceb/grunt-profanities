0ed9d48998dc16aa7bac406e682ae7c56bf7fba6
Gabriel Cebrian <gabceb@gmail.com>
2013-11-22 17:37:41 -0800

    Removed comments from profanities grunt test

--- test/profanities_test.js
+++ test/profanities_test.js
@@ -3,26 +3,6 @@
 var grunt = require('grunt');
 var profanities = require('../tasks/profanities');
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-
 exports.profanities = {
   setUp: function(done) {
     done();
